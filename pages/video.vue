3a941c7061498a66ef9b17dc0d5d6a9e27b0abae
4ster-light <4ster.dev@proton.me>
2025-03-23 22:44:02 +0100

    Fixed mobile text responsiveness on /video

--- pages/video.vue
+++ pages/video.vue
@@ -1,8 +1,10 @@
 <template>
   <main class="flex flex-col items-center justify-center h-[87vh]">
-    <h1 class="text-7xl mb-10 font-bold text-transparent bg-clip-text bg-gradient-to-r from-mauve to-blue leading-tight">
+    <h1 class="text-5xl md:text-7xl mb-10 font-bold text-transparent bg-clip-text bg-gradient-to-r from-mauve to-blue leading-tight">
       ✰ Coming soon ✰
     </h1>
-    <NuxtLink to="/" class="p-4 font-semibold rounded-lg bg-surface0 hover:bg-surface1 transition-colors duration-150 ease-in-out text-2xl">Back Home &rarr;</NuxtLink>
+    <NuxtLink to="/" class="p-4 font-semibold rounded-lg bg-surface0 hover:bg-surface1 transition-colors duration-150 ease-in-out text-2xl">
+      Back Home &rarr;
+    </NuxtLink>
   </main>
 </template>
