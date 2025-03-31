fe6fe04d21162b444f0294571d6f33fa5f1aa141
4ster-light <4ster.dev@proton.me>
2025-03-31 17:43:04 +0200

    Second attempt and changed site name

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 useSeoMeta({
-	title: "✰ArtSCII✰",
+	title: "✰ArTXT✰",
 	description: "The best image to ascii art converter",
-	ogTitle: "Artscii",
+	ogTitle: "ArTXT",
 	ogDescription: "The best image to ascii art converter",
 	ogImage: "/Icon.svg",
-	ogUrl: "https://artscii.deno.dev",
-	twitterTitle: "Artscii",
+	ogUrl: "https://Artxt.netlify.app",
+	twitterTitle: "ArTXT",
 	twitterDescription: "The best image to ascii art converter",
 	twitterImage: "/Icon.svg",
 	twitterCard: "summary",
